d14bb16a0f6842a4933ffce08ab1f865806d84d0
alexis villaraza <alexis.borja@gmail.com>
2020-01-30 13:29:13 -0700

    added yup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './component/Form';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Form />
           Learn React
         </a>
       </header>
--- my-app/src/component/Form.js
+++ my-app/src/component/Form.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import {withFormik, Field} from 'formik';
+import React, { useEffect } from 'react';
+import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
 
-const Form = ({values}) => {
+const Form = ({values, errors, touched}) => {
     
     return (
         <div>
@@ -10,19 +11,23 @@ const Form = ({values}) => {
                     Name:
                 </label>
                 <Field id='name' type='text' name='name'/>
+                {touched.name & errors.name && <p>{errors.name}</p>}
 
                 <label htmlFor='email'>
                     Email:
                 </label>
                 <Field id='email' type='email' name='email'/>
+                {touched.email & errors.email && <p>{errors.email}</p>}
 
                 <label htmlFor='password'>
                     Password:
                 </label>
                 <Field id='password' type='password' name='password' />
+                {touched.password & errors.password && <p>{errors.password}</p>}
 
                 <label htmlFor='terms'>Terms of Service
                 <Field id='terms' type='checkbox' name='terms' check={values.terms} />
+                {touched.terms & errors.terms && <p>{errors.terms}</p>}
                 </label>
 
                 <button type='submit'>Submit</button>
@@ -31,10 +36,24 @@ const Form = ({values}) => {
             </Form>
         </div>
     )
-}
+};
 
-// Name
-//  Email
-//  Password
-//  Terms of Service (checkbox)
-//  A Submit button to send our form data to the server.
+const FormikAnimalForm = withFormik({
+    mapPropsToValues({name, email, }) {
+        return {
+            name: name || '',
+            email: '',
+            terms: terms || false,
+
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required(),
+        terms: yup.boolean().oneOf([true], 'Must Accept Terms of Service'),
+    }),
+})
+
+
+export default FormikForm;
